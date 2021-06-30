bc93476c889a8e59fe6c4f952eff5b1e889e99ac
Clarke Hardy <cahardy@stanford.edu>
2021-06-30 13:32:06 -0700

    added start of job number range

--- Lightmap/create_lm_jobs.py
+++ Lightmap/create_lm_jobs.py
@@ -1,12 +1,14 @@
 import subprocess
 
+first_num = 0
+
 args = []
 with open('jobs_list.txt','r') as infile:
     for line in infile:
         args.append(line)
 
 for i in range(len(args)):
-    with open('lm_recon'+str(i+1)+'.sh','w') as newfile:
+    with open('lm_recon'+str(i+first_num)+'.sh','w') as newfile:
         newfile.write('python3 roundtrip.py '+args[i][:-1]+' -train -input_files $SIM_DIR/g4andcharge_10kevts_xe127_seed*_REDUCED.pkl')
 
 subprocess.Popen('chmod +x lm_recon*.sh',shell=True).wait()
